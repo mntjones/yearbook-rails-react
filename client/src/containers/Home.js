234e1256267a4196fdc9c0f27573ebd5212e68f4
Monica <mntamaru@gmail.com>
2019-07-27 19:13:38 -0700

    updated App views

--- client/src/containers/Home.js
+++ client/src/containers/Home.js
@@ -2,9 +2,15 @@ import React from 'react';
 
 const Home = () => {
   return(
-    <div className='App-header'>
-      <h1>Welcome to Yearbook!</h1>
-      <p> Checkout the Members tab to see who's in your yearbook!</p>
+    <div>
+    	<div className='App-header'>
+      	<h1>Welcome to Yearbook!</h1>
+    	</div>
+    	<div className='App-body'>
+      	<p> Checkout the Members tab to see who's in your yearbook!</p>
+
+      	<p> Add, Edit, and Delete Members of your yearbook! Keep track of your favorite people!</p>
+    	</div>
     </div>
   )
 }
